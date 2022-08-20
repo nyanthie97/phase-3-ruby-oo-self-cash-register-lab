e781f879d11cd44356c5e1f0a82d0c876bc02de5
Nyanthie Mayang <nyanthie.mayang@student.moringaschool.com>
2022-08-21 02:56:30 +0300

    commit tree 5d81ff781e9870b61ac71361212e5b3359835ec4

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,44 @@
+require 'pry'
 
+class CashRegister 
+    attr_accessor :total, :discount, :previous_total, :title
+
+    def initialize (discount = 0)  
+        @total = 0
+        @previous_total = 0
+        @discount = discount
+        @title = []
+    end  
+
+    def add_item (title, price, quantity = 1)
+        self.total += (price * quantity)
+        self.previous_total = self.total - (price * quantity)
+        if quantity == 1
+            @title.push(title)
+        elsif quantity > 1
+            items = ("#{title} " * quantity).split(" ")
+            items.each {|item| @title.push(item)}
+        end
+    end 
+
+    def apply_discount 
+        if self.discount > 0 
+            self.total = self.total - (self.total * (discount.to_f / 100.0))
+            return "After the discount, the total comes to $#{self.total.to_i}."
+        else
+            return "There is no discount to apply."
+        end
+    end
+
+    def items
+        @title
+    end
+
+    def void_last_transaction 
+        if self.title.length > 0
+            self.total = self.previous_total
+        else
+            self.total = 0
+        end
+    end
+end
